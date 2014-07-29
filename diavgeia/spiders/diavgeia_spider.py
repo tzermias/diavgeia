927698f560001e9d28fec8dc1fc13db91895280d
tzermias <zacharias.tzermias@gmail.com>
2014-07-29 11:51:06 +0300

    Get search  arguments from command line.
    Get search terms from command line (using -a flag) and create the
    appropriate request to the API.
    The argument passed to the command line must be one of the following:
    "ada", "org", "subject", "protocol", "term", "unit", "signer","type",
    "tag", "from_date", "to_date", "from_issue_date", "status", "size".
    If an argument is passed to the spider and does not exist to the
    aforementioned list, a warning is issued on the log.
    If no arguments are supplied to the spider, the spider requests the
    decisions of the previous day.

--- diavgeia/spiders/diavgeia_spider.py
+++ diavgeia/spiders/diavgeia_spider.py
@@ -4,6 +4,7 @@ from scrapy import log
 from scrapy.selector import XmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.http.request import Request
+from datetime import date, timedelta
 
 class DiavgeiaSpider(BaseSpider):
     """ DiavgeiaSpider
@@ -12,14 +13,41 @@ class DiavgeiaSpider(BaseSpider):
     """
     name = "diavgeia_spider"
     allowed_domains = ["diavgeia.gov.gr"]
+    allowed_arguments = (
+            "ada", "org", "subject", "protocol", "term", "unit", "signer",
+            "type", "tag", "from_date", "to_date", "from_issue_date", "status",
+            "size"
+        )
     base_url = "https://test3.diavgeia.gov.gr/luminapi/opendata/"
 
     def __init__(self , *args, **kwargs):
         super(DiavgeiaSpider, self).__init__(*args, **kwargs)
         self.log("Initialized DiavgeiaSpider object", level=log.DEBUG)
-        self.url = self.base_url + \
-        "search?from_date=01-01-2014&to_date=02-01-2014&size=100&page=%s"
-        self.start_urls = [ self.url % (0,)]
+        # Parse arguments.
+        # Currently legal arguments are ada, subject, protocol, term, unit,
+        # signer, type, tag, from_date, to_date, from_issue_date, status and size
+        # If no arguments are specified, then the crawler retrieves every
+        # decision submitted the previous day.
+        if len(kwargs) > 0:
+            args_array = []
+            for key in kwargs:
+                if key in self.allowed_arguments:
+                    args_array.append("%s=%s" % (key, kwargs[key]))
+                else:
+                    self.log("Argument %s is not a valid argument" % key,
+                            level=log.WARNING) 
+            lala = "&".join(args_array) 
+            self.log("Arguments: %s" % lala, level=log.DEBUG)
+            self.url =self.base_url + "search?" + lala + "&page=%s"
+            self.start_urls = [ self.url % (kwargs['page'] if 'page' in
+                kwargs.keys() else 0,)]
+            self.log("Request: %s" % self.start_urls[0], level=log.DEBUG)
+        else:
+            from_date = (date.today() - timedelta(1))
+            self.url = self.base_url + \
+            "search?from_date=%s&to_date=%s&size=100&page=%%s" % (from_date,
+                    date.today())
+            self.start_urls = [ self.url % (0,)]
 
     def parse (self, response):
         xxs = XmlXPathSelector(response)
@@ -27,6 +55,10 @@ class DiavgeiaSpider(BaseSpider):
 
         # Parse Decision
         decisions = xxs.select("//xmlns:decisions/xmlns:decision")
+
+        # Print debug the query field of the XML
+        query = xxs.select("//xmlns:info/xmlns:query/text()").extract()[0]
+        self.log("Query: %s" % query, level=log.DEBUG)
         for decision in decisions:
             d = DiavgeiaItem()
             for element in decision.select("*"):
